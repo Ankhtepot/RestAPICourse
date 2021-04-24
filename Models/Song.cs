8264a18d623ec3e751e0dc994a6e863f8bc27971
PetrZavodny <petr.zavodny@chyronhego.com>
2021-04-24 17:28:10 +0200

    Up to lesson 53.

--- Models/Song.cs
+++ Models/Song.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,10 +12,18 @@ namespace MusicApi.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Language { get; set; }
         public string Duration { get; set; }
+        public DateTime UploadedDate { get; set; }
+        public bool IsFeatured { get; set; }
+
         [NotMapped]
         public IFormFile Image { get; set; }
         public string ImageUrl { get; set; }
+        [NotMapped]
+        public IFormFile AudioFile { get; set; }
+        public string AudioUrl { get; set; }
+        public int ArtistId { get; set; }
+        public int? AlbumId { get; set; }
+
     }
 }
